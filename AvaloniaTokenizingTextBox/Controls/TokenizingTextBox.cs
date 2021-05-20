b5c3d287aca132d06991fb00c7ada2fce5a8fc35
Scott Whitney <scottwhits@gmail.com>
2021-05-20 23:02:19 +0930

    TextBox adds Token strings to a list

---
+++ AvaloniaTokenizingTextBox/Controls/TokenizingTextBox.cs
@@ -0,0 +1,125 @@
+﻿using Avalonia;
+using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using System;
+using System.Collections.Generic;
+
+namespace AvaloniaTokenizingTextBox.Controls
+{
+    public class TokenizingTextBox : ListBox
+    {
+        private const string PART_TextBox = "PART_TextBox";
+        private const string PART_Border = "PART_Border";
+        private TextBox? _textBox;
+
+        /// <summary>
+        /// Gets or sets SearchText.
+        /// </summary>
+        public string InputText
+        {
+            get { return (string)GetValue(InputTextProperty); }
+            set { SetValue(InputTextProperty, value); }
+        }
+
+        /// <summary>
+        /// Defines the SearchText property.
+        /// </summary>
+        public static readonly StyledProperty<string> InputTextProperty =
+        AvaloniaProperty.Register<TokenizingTextBox, string>(nameof(InputText));
+
+        public TokenizingTextBox()
+        {
+            SelectionModeProperty.OverrideMetadata(typeof(TokenizingTextBox), new StyledPropertyMetadata<SelectionMode>(SelectionMode.Multiple));
+        }
+
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            base.OnApplyTemplate(e);
+
+            if (_textBox != null)
+            {
+                _textBox.KeyDown -= TextBox_KeyDown;
+                _textBox.RemoveHandler(TextInputEvent, TextBox_TextChanged);
+            }
+
+            _textBox = (TextBox)e.NameScope.Get<Control>(PART_TextBox);
+
+            if (_textBox != null)
+            {
+                _textBox.KeyDown += TextBox_KeyDown;
+                _textBox.AddHandler(TextInputEvent, TextBox_TextChanged, RoutingStrategies.Tunnel);
+            }
+        }
+
+        private void TextBox_TextChanged(object? sender, TextInputEventArgs e) 
+        {
+            string t = _textBox.Text;
+
+            if (t == null) return;
+
+            if (!string.IsNullOrEmpty(" ") && t.Contains(" "))
+            {
+                bool lastDelimited = t[t.Length - 1] == " "[0];
+
+                string[] tokens = t.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                int numberToProcess = lastDelimited ? tokens.Length : tokens.Length - 1;
+                for (int position = 0; position < numberToProcess; position++)
+                {
+                    AddToken(tokens[position]);
+                }
+
+                if (lastDelimited)
+                {
+                    _textBox.Text = string.Empty;
+                }
+                else
+                {
+                    _textBox.Text = tokens[tokens.Length - 1];
+                    _textBox.CaretIndex = _textBox.Text.Length;
+                }
+            }
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+
+            base.OnKeyDown(e);
+        }
+
+        private void TextBox_KeyDown(object? sender, KeyEventArgs e) { }
+
+        private void AddToken(string token) 
+        {
+            if (token.Length > 0)
+            {
+                var tokens = new AvaloniaList<string>();
+
+                foreach (var item in Items)
+                {
+                    tokens.Add((string)item);
+                }
+
+                tokens.Add(token);
+
+                Items = tokens;
+            }
+
+        }
+
+        protected override void ItemsChanged(AvaloniaPropertyChangedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Create Token?");
+            base.ItemsChanged(e);
+        }
+
+        private void AddToken() 
+        {
+            var text = _textBox.Text;
+            _textBox.Text = string.Empty;
+            AddToken(text);
+        }
+    }
+}
---
+++ AvaloniaTokenizingTextBox/Controls/TokenizingTextBoxItem.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaTokenizingTextBox.Controls
+{
+    public class TokenizingTextBoxItem : ListBoxItem
+    {
+    }
+}
